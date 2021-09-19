dd723c76356f5df7bb82d4a970cd834619f49207
Hannah Park <ha3158987@gmail.com>
2021-09-20 01:11:16 +0900

    fix: DatesOfMonth 내부 계산로직 useCalendar로 이동 (#13)

--- src/Components/Calendar/DatesOfMonth.jsx
+++ src/Components/Calendar/DatesOfMonth.jsx
@@ -1,69 +1,15 @@
-import * as moment from "moment";
 import styled from "styled-components";
 import useCalendar from "Hooks/useCalendar";
 
 const DatesOfMonth = () => {
-  const { currYear, currMonth, today, selectedDate, handleClickedDate } =
-    useCalendar();
-
-  //보여지는 월의 마지막 날짜
-  const lastDateOfCurrMonth = moment([currYear, 0, 31])
-    .month(currMonth - 1)
-    .format("DD");
-
-  //이번달 1일의 요일
-  const firstDayOfThisMonth = moment([currYear, currMonth - 1, 1]).day();
-
-  //이번달 마지막날의 요일
-  const lastDayOfThisMonth = moment([
+  const {
     currYear,
-    currMonth - 1,
-    lastDateOfCurrMonth,
-  ]).day();
-
-  //이전달의 마지막 날짜
-  const lastDateOfLastMonth = moment([currYear, 0, 31])
-    .month(currMonth - 1 - 1)
-    .format("DD");
-
-  //보여지는 월의 1 ~ 마지막날짜까지 일자를 갖고 있는 배열
-  const datesOfCurrMonth = Array.from(
-    { length: lastDateOfCurrMonth },
-    (v, i) => {
-      return {
-        date: i + 1,
-        month: "current",
-      };
-    }
-  );
-
-  const getDatesOfLastMonth = () => {
-    const emptyArr = Array.from({ length: firstDayOfThisMonth });
-    let firstShowingDate = lastDateOfLastMonth - firstDayOfThisMonth + 1;
-    return emptyArr.map(() => {
-      return {
-        date: firstShowingDate++,
-        month: "previous",
-      };
-    });
-  };
-
-  const getDatesOfNextMonth = () => {
-    const emptyArr = Array.from({ length: 6 - lastDayOfThisMonth });
-    let count = 1;
-    return emptyArr.map(() => {
-      return {
-        date: count++,
-        month: "next",
-      };
-    });
-  };
-
-  const datesOfMonth = [
-    ...getDatesOfLastMonth(),
-    ...datesOfCurrMonth,
-    ...getDatesOfNextMonth(),
-  ];
+    currMonth,
+    today,
+    selectedDate,
+    datesOfMonth,
+    handleClickedDate,
+  } = useCalendar();
 
   return (
     <DatesOfMonthLayout>
--- src/Hooks/useCalendar.js
+++ src/Hooks/useCalendar.js
@@ -1,11 +1,72 @@
+import * as moment from "moment";
 import { useDispatch, useSelector } from "react-redux";
 import { clickDate } from "ReduxStore/modules/calendar";
 
 const useCalendar = () => {
+  const dispatch = useDispatch();
   const { currYear, currMonth, today, selectedDate } = useSelector(
     (state) => state
   );
-  const dispatch = useDispatch();
+
+  //보여지는 월의 마지막 날짜
+  const lastDateOfCurrMonth = moment([currYear, 0, 31])
+    .month(currMonth - 1)
+    .format("DD");
+
+  //이번달 1일의 요일
+  const firstDayOfThisMonth = moment([currYear, currMonth - 1, 1]).day();
+
+  //이번달 마지막날의 요일
+  const lastDayOfThisMonth = moment([
+    currYear,
+    currMonth - 1,
+    lastDateOfCurrMonth,
+  ]).day();
+
+  //이전달의 마지막 날짜
+  const lastDateOfLastMonth = moment([currYear, 0, 31])
+    .month(currMonth - 1 - 1)
+    .format("DD");
+
+  //보여지는 월의 1 ~ 마지막날짜까지 일자를 갖고 있는 배열
+  const datesOfCurrMonth = Array.from(
+    { length: lastDateOfCurrMonth },
+    (v, i) => {
+      return {
+        date: i + 1,
+        month: "current",
+      };
+    }
+  );
+
+  const getDatesOfLastMonth = () => {
+    const emptyArr = Array.from({ length: firstDayOfThisMonth });
+    let firstShowingDate = lastDateOfLastMonth - firstDayOfThisMonth + 1;
+    return emptyArr.map(() => {
+      return {
+        date: firstShowingDate++,
+        month: "previous",
+      };
+    });
+  };
+
+  const getDatesOfNextMonth = () => {
+    const emptyArr = Array.from({ length: 6 - lastDayOfThisMonth });
+    let count = 1;
+    return emptyArr.map(() => {
+      return {
+        date: count++,
+        month: "next",
+      };
+    });
+  };
+
+  const datesOfMonth = [
+    ...getDatesOfLastMonth(),
+    ...datesOfCurrMonth,
+    ...getDatesOfNextMonth(),
+  ];
+
   const handleClickedDate = (clickedDate, displayedMonth) => {
     dispatch(
       clickDate({
@@ -14,7 +75,20 @@ const useCalendar = () => {
       })
     );
   };
-  return { currYear, currMonth, today, selectedDate, handleClickedDate };
+  return {
+    currYear,
+    currMonth,
+    today,
+    selectedDate,
+    lastDateOfCurrMonth,
+    firstDayOfThisMonth,
+    lastDayOfThisMonth,
+    datesOfCurrMonth,
+    getDatesOfLastMonth,
+    getDatesOfNextMonth,
+    datesOfMonth,
+    handleClickedDate,
+  };
 };
 
 export default useCalendar;
